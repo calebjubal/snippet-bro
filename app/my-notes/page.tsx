755339c2eb183b26899b95d6f6823d9e0583c79d
calebjubal <calebjubal@outlook.com>
2024-07-10 19:26:45 +0530

    logout button

--- app/my-notes/page.tsx
+++ app/my-notes/page.tsx
@@ -1,9 +1,10 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      hello
+      <UserButton />
     </div>
   )
 }
