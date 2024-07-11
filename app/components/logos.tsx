98666ae2d1deb45c9d2ee89c45f6588b863fbb5e
calebjubal <calebjubal@outlook.com>
2024-07-11 13:10:26 +0530

    struc update

---
+++ app/components/logos.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import DataObjectIcon from '@mui/icons-material/DataObject'
+
+function Logo() {
+    return (
+      <div className="flex gap-2 items-center">
+          <div className={`bg-[#FF3366] p-[6px] rounded-md`}>
+              <DataObjectIcon sx={{ fontSize: 27, color: "white" }} />
+          </div>
+          <div className="flex gap-1 text-[19px]">
+              <span className={`font-bold text-[#FF3366]`}>Snippet</span>
+              <span className='text-white-600'>Bro</span>
+          </div>
+      </div>
+    )
+  }
+
+export default Logo
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 import Hero from './components/hero'
-import DataObjectIcon from '@mui/icons-material/DataObject'
-import Link from 'next/link';
-import { useAuth } from '@clerk/nextjs';
 import Buttons from './components/buttons';
+import Logo from './components/logos';
 
 export default function Home() {
   return (
@@ -23,16 +21,3 @@ function Navbar() {
   )
 }
 
-function Logo() {
-  return (
-    <div className="flex gap-2 items-center">
-        <div className={`bg-[#FF3366] p-[6px] rounded-md`}>
-            <DataObjectIcon sx={{ fontSize: 27, color: "white" }} />
-        </div>
-        <div className="flex gap-1 text-[19px]">
-            <span className={`font-bold text-[#FF3366]`}>Snippet</span>
-            <span className='text-white-600'>Bro</span>
-        </div>
-    </div>
-  )
-}
